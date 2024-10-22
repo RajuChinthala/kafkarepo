110aef80eebfc0d88674a54223a7842d2e907814
rajuchinthala <rajuchinthala.89@gmail.com>
2024-10-21 20:27:09 -0400

    Added the ibmmq branch

---
+++ demo-ibmmq/src/test/java/com/example/ibmmq/demo_ibmmq/DemoIbmmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibmmq.demo_ibmmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIbmmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
